34fe3de0bae35f7ef01889d52810067efff3cafa
mhayescs19 <mhayescs19@gmail.com>
2025-04-23 13:36:06 -0700

    update font class for sizing

--- features/navbar/components/logo.tsx
+++ features/navbar/components/logo.tsx
@@ -2,8 +2,8 @@ export default function Logo() {
   return (
     <div className="flex flex-row gap-2 items-center">
       <a href="/" className="flex flex-row gap-1 items-center">
-        <p className="factoExtraBold font-normal text-xl">UNIQLO</p>
-        <div className="factoExtraBold w-[24px] h-[30px] bg-black rounded-xs text-white flex items-center justify-center">
+        <p className="factoExtraBold-heading font-normal">UNIQLO</p>
+        <div className="factoExtraBold-heading w-[24px] h-[30px] bg-black rounded-xs text-white flex items-center justify-center">
           2
         </div>
       </a>
