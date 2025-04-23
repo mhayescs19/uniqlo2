c056af7ff567b6f21d168c6691ae9528b7a72d73
mhayescs19 <mhayescs19@gmail.com>
2025-04-23 11:05:27 -0700

    add navbar links

--- features/navbar/components/navbar.tsx
+++ features/navbar/components/navbar.tsx
@@ -2,8 +2,15 @@ import Logo from "@/features/navbar/components/logo";
 
 export default function Navbar() {
   return (
-    <nav className="sticky top-0 z-50 bg-white/90 backdrop-blur-sm h-20 flex justify-between items-center pl-20">
+    <nav className="pl-20 pr-20 sticky top-0 z-50 bg-white/90 backdrop-blur-sm h-20 flex justify-between items-center">
       <Logo />
+      <div className="flex gap-12 items-center">
+        <a href="/stats">Stats</a>
+        <a href="/purchase">Purchase</a>
+        <a className="font-normal" href="/manage">
+          Manage
+        </a>
+      </div>
     </nav>
   );
 }
