963ecb17047e7d2ea6b2423575806175687598ef
mhayescs19 <mhayescs19@gmail.com>
2025-04-24 15:39:28 -0700

    add error handling for fields

--- features/manage-inventory/components/add-inventory-card.tsx
+++ features/manage-inventory/components/add-inventory-card.tsx
@@ -26,7 +26,10 @@ enum FitStyles {
 
 export default function AddInventoryCard() {
   const fields = new ProductForm();
+  const fieldErrors = new ProductFormErrors();
+
   const [formData, setFormData] = useState(fields);
+  const [errors, setErrors] = useState(fieldErrors);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setFormData((prev) => ({
@@ -34,12 +37,37 @@ export default function AddInventoryCard() {
       [e.target.name]: e.target.value, // overwrite the updated inputs by using e (e is the event object that contains changes to the input field)
     }));
   };
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault(); // prevent immediate page refresh; allows custom logic
+
+    const currErrors = validate();
+    if (currErrors.isErrors()) {
+      console.log("setting errors");
+      console.log(currErrors);
+      setErrors(currErrors);
+      return;
+    }
+  };
+
+  const validate = () => {
+    const errors = new ProductFormErrors();
+
+    if (
+      !formData.productName.trim() ||
+      formData.productName.trim().length < 2
+    ) {
+      console.log("invalid product name");
+      errors.productName = "Name must be at least 2 characters";
+    }
+
+    return errors;
+  };
 
   return (
     <div>
       <div className="factoExtraBold-title text-left">ADD PRODUCT</div>
       <div className="w-full flex rounded-md border-black">
-        <form className="grid grid-cols-2 gap-6">
+        <form className="flex flex-col gap-3.75 pt-2" onSubmit={handleSubmit}>
           <div className="flex flex-col">
             <label className="pb-1.75 font-bold">Name</label>
             <input
@@ -50,7 +78,16 @@ export default function AddInventoryCard() {
               value={formData.productName}
               onChange={handleChange}
             ></input>
+            {errors.productName !== "" && (
+              <div className="pt-1 text-red-700 text-[0.7rem]">
+                {errors.productName}
+              </div>
+            )}
           </div>
+          </div>
+          <button type="submit" className="cursor-pointer">
+            Add to inventory
+          </button>
         </form>
       </div>
     </div>
