96ef5e5fcbd3c24ecd9ec9c383027cdfccf59eba
mhayescs19 <mhayescs19@gmail.com>
2025-04-23 22:43:24 -0700

    add custom tw colors

---
+++ tailwind.config.ts
@@ -0,0 +1,15 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  theme: {
+    extend: {
+      colors: {
+        "outline-gray": "#c6c6c6",
+        "outline-gray-light": "#e3e8f0",
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;
