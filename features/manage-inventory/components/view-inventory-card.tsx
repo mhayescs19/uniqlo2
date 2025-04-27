336e094e0ed806242cc8d54c431f36853e9b9e01
mhayescs19 <mhayescs19@gmail.com>
2025-04-27 13:12:43 -0700

    refactor table from one div styled td per row to multiple styled tds

--- features/manage-inventory/components/view-inventory-card.tsx
+++ features/manage-inventory/components/view-inventory-card.tsx
@@ -5,34 +5,36 @@ export default function ViewInventoryCard() {
   return (
     <div>
       <div className="w-full flex rounded-md mt-9.5">
-        <div className="flex flex-col gap-3.75 max-w-sm mx-auto border border-outline-gray-light-4 p-4 rounded-[0.3125rem]">
-          <table className="table-auto w-full border-separate border-spacing-1">
+        <div className="flex flex-col gap-3.75 mx-auto border border-outline-gray-light-4 p-1 pl-3 pr-3 rounded-[0.3125rem]">
+          <table className="table-auto w-full border-separate border-spacing-y-3">
             <thead>
               <tr>
-                <th className="px-4 py-2 text-left">SKU</th>
-                <th className="px-4 py-2 text-left">Type</th>
-                <th className="px-4 py-2 text-left">Name</th>
-                <th className="px-4 py-2 text-left">Price</th>
+                <th className="px-4 text-left">SKU</th>
+                <th className="px-4 text-left">Fit</th>
+                <th className="px-4 text-left">Name</th>
+                <th className="px-4 text-left">Price</th>
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <td colSpan={5} className="p-0">
-                  <div className="grid grid-cols-4 items-center gap-6 border border-gray-300 rounded-md p-4">
-                    <span className="w-1/4">000</span>
-                    <span className="w-1/4">UX</span>
-                    <span className="w-10">3D Knit Sweater</span>
-                    <span className="flex items-center">
-                      $75.00
-                      <span className="flex items-cente ml-2.25">
-                        <button className="cursor-pointer">
-                          <Trash size={24} />
-                        </button>
-                      </span>
-                    </span>
-                  </div>
-                </td>
-              </tr>
+              {items.map((item) => (
+                <tr key={item.sku} className="">
+                  <td className="p-4 border-2 border-r-0 border-gray-light rounded-l-[0.3125rem]">
+                    {item.sku}
+                  </td>
+                  <td className="p-4 border-2 border-l-0 border-r-0 border-gray-light">
+                    {item.fit}
+                  </td>
+                  <td className="p-4 border-2 border-l-0 border-r-0 border-gray-light">
+                    {item.name}
+                  </td>
+                  <td className="p-4 flex items-center justify-between border-2 border-l-0 border-gray-light rounded-r-[0.3125rem]">
+                    <span>${item.price}</span>
+                    <button className="ml-2 cursor-pointer">
+                      <Trash size={24} />
+                    </button>
+                  </td>
+                </tr>
+              ))}
             </tbody>
           </table>
         </div>
