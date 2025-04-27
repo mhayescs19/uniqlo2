664497518dc24a577a3b72d4939358d45c9a8fcf
mhayescs19 <mhayescs19@gmail.com>
2025-04-26 22:39:23 -0700

    remove navbar blur

--- features/navbar/components/navbar.tsx
+++ features/navbar/components/navbar.tsx
@@ -2,7 +2,7 @@ import Logo from "@/features/navbar/components/logo";
 
 export default function Navbar() {
   return (
-    <nav className="pl-20 pr-20 sticky top-0 z-50 bg-white/90 backdrop-blur-sm h-20 flex justify-between items-center">
+    <nav className="pl-20 pr-20 sticky top-0 z-50 bg-white/90 h-20 flex justify-between items-center">
       <Logo />
       <div className="flex gap-12 items-center">
         <a href="/stats">Stats</a>
