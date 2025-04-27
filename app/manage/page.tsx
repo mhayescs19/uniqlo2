9878c3982ac1776a747607b10e9a5202c04ff252
mhayescs19 <mhayescs19@gmail.com>
2025-04-26 22:38:21 -0700

    add view inventory card to manage page

--- app/manage/page.tsx
+++ app/manage/page.tsx
@@ -1,12 +1,13 @@
 "use client";
 
 import AddInventoryCard from "@/features/manage-inventory/components/add-inventory-card";
+import ViewInventoryCard from "@/features/manage-inventory/components/view-inventory-card";
 
 export default function Manage() {
   return (
     <div className="flex flex-row justify-center gap-11 pt-14">
       <AddInventoryCard />
-      <div>Card2</div>
+      <ViewInventoryCard />
     </div>
   );
 }
