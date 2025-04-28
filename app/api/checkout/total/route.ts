6b94ef5231e16acb00e6022360a29d928673b233
mhayescs19 <mhayescs19@gmail.com>
2025-04-28 16:49:37 -0700

    add explicit full request url

--- app/api/checkout/total/route.ts
+++ app/api/checkout/total/route.ts
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server";
 import { supabase } from "@/config/supabase";
+import { SITE_BASE_URL } from "@/config/config";
 /**
  *
  * @param request list of product uuids
@@ -54,16 +55,19 @@ async function createSubtotal(request: Request) {
     console.log(subTotal);
 
     const webhookPayload = {
-      tagIds: tagIds,
+      tagIds: tags,
     };
 
-    const webhookReponse = await fetch("/api/checkout/purchase/details", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(webhookPayload),
-    });
+    const webhookReponse = await fetch(
+      `${SITE_BASE_URL}/api/checkout/purchase/details`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(webhookPayload),
+      }
+    );
 
     if (!webhookReponse.ok) {
       const val = await webhookReponse.json();
