e28f7fdca54e7377b55726f430e193d4a7688615
mhayescs19 <mhayescs19@gmail.com>
2025-04-28 16:31:07 -0700

    turn off linting during dev phase

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    // skips ESLint checks during `next build` and `next dev`
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
