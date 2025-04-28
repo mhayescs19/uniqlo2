bcacf215750aefa9817cdb08c86160e81dae7903
mhayescs19 <mhayescs19@gmail.com>
2025-04-28 10:28:28 -0700

    add rpc function to get total sales

---
+++ app/api/stats/transaction/sales/value/route.ts
@@ -0,0 +1,32 @@
+import { NextResponse } from "next/server";
+import { supabase } from "@/config/supabase";
+
+async function getTotalSales() {
+  try {
+    /*
+      create or replace function total_sales_to_date()
+      returns numeric
+      language sql
+      as $$
+        select sum(p.price)
+        from transaction t
+        join product p on t.product_id = p.id;
+      $$;
+    */
+    const { data, error } = await supabase.rpc("total_sales_to_date"); // invokes custom sql function
+
+    console.log(data);
+
+    if (error) throw new Error(error.message);
+
+    const payload = {
+      totalSales: data,
+    };
+
+    return NextResponse.json(payload);
+  } catch (error) {
+    return NextResponse.json({ error: error }, { status: 400 });
+  }
+}
+
+export const GET = getTotalSales;
