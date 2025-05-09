11aec021872b71e838db7949dca781bc8f8fdd93
mhayescs19 <mhayescs19@gmail.com>
2025-05-09 11:11:43 -0700

    add colors to tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,10 @@ const config: Config = {
       colors: {
         "outline-gray": "#c6c6c6",
         "outline-gray-light": "#e3e8f0",
+        ublue: "#70abf5",
+        "ublue-stroke": "#417cc6",
+        "ublue-stroke-secondary": "#244976",
+        ugray: "#6d6d6d",
       },
     },
   },
